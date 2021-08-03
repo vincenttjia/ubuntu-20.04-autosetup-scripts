4d181de4bb7de6f78bed2dbe49c0ee21de3ecb44
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-08-03 19:15:47 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 sudo apt update
 sudo apt install -y redis-server
 sudo systemctl enable redis-server
+sudo sed -i 's/bind 127\.0\.0\.1 ::1/bind 0\.0\.0\.0/g' /etc/redis/redis.conf
+sudo sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf
 sudo bash -c 'echo "maxmemory 512mb" >> /etc/redis/redis.conf'
 sudo bash -c 'echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf'
 sudo systemctl restart redis-server
