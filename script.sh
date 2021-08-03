895009d9514be4618f53bfab3a3163b9a0995c01
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-08-03 19:01:54 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y redis-server
+sudo systemctl enable redis-server
+sudo bash -c 'echo "maxmemory 512mb" >> /etc/redis/redis.conf'
+sudo bash -c 'echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf'
+sudo systemctl restart redis-server
